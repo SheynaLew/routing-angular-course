87dac502215f4bd69bab92a745e856aa56c43063
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-08-09 15:30:38 +0100

    practicing and come common gotchas

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -16,12 +16,12 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id']
+    const id = +this.route.snapshot.params['id'] //using +params['id'] changes the id from a string to a number so that the URL can be read correctly
 
     this.server = this.serversService.getServer(id);
 
     this.route.params.subscribe((params:Params) => {
-      this.server = this.serversService.getServer(+params['id']);
+      this.server = this.serversService.getServer(+params['id']); //using +params['id'] changes the id from a string to a number so that the URL can be read correctly
     })
   }
 
