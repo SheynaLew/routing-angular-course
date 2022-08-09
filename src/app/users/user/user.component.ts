c649b08d291f1c1a8c38d2511f98beadf710436a
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-08-09 15:07:58 +0100

    fetching route perameters reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -18,6 +18,12 @@ export class UserComponent implements OnInit, OnDestroy {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    // .params is giving us access to the parameters we defined in app.module. In this case, id and name (i.e: users/:id/:name)
+    // we can output that data in the HTML as {{ user.name }}
+    // snapshot only works if we haven't been on the route before. So if we have a link to a user on the users page, it won't update the HTML to show the new user's name and ID (i.e 10, Anna). Snapshot works for the initial page load, but it is not dynamic enough for this purpose.
+      //Instead we can subscribe to params as an observable
+        // When params is updated, it nudges user.id and user.name to let them know to change their value
+        // See below (pass in params to subscribe so we have access to the updated parameters)
 
     this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
