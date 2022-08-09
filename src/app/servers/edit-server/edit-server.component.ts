348b6345db5db5c4b56f76a7f3fa0662a1dcaf2c
Jenny Lewis-Gandy <jennylewis-gandy@Jennys-MacBook-Pro.local>
2022-08-09 16:01:11 +0100

    using query params practice

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -9,14 +9,19 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./edit-server.component.css']
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.queryParams.subscribe(); //using subscribe on queryParams means they will be updated reactively. If we use snapshot it will only load the inital state. Same for fragment
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    ); // using subscribe on queryParams means they will be updated reactively. If we use snapshot it will only load the inital state. Same for fragment
     this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
@@ -24,7 +29,7 @@ export class EditServerComponent implements OnInit {
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
   }
 
 }
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { ServersService } from '../servers.service';
@@ -10,19 +10,23 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-  paramsSubscription:Subscription;
+  server: { id: number, name: string, status: string };
+  paramsSubscription: Subscription;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'] //using +params['id'] changes the id from a string to a number so that the URL can be read correctly
+    const id = +this.route.snapshot.params['id']; // using +params['id'] changes the id from a string to a number so that the URL can be read correctly
 
     this.server = this.serversService.getServer(id);
 
-    this.route.params.subscribe((params:Params) => {
-      this.server = this.serversService.getServer(+params['id']); //using +params['id'] changes the id from a string to a number so that the URL can be read correctly
-    })
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']); // using +params['id'] changes the id from a string to a number so that the URL can be read correctly
+    });
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 
 }
