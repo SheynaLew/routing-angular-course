8c3a66c974bf1347353e410fb7b69dabb06ff858
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-08-30 15:27:08 +0100

    redirecting notes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,8 +31,13 @@ const appRoutes: Routes = [
     ]
   },
   { path: "not-found", component: PageNotFoundComponent },
-  { path: "**", redirectTo: '/not-found' }
+  { path: "**", redirectTo: '/not-found', pathMatch: 'full' }
   // ** is a wildcard which captures any routes not specified above. As such, it needs to be the last route in the set up or anything listed below it will be sent to the component/redirect route you've assigned to it.
+  // In our example, we didn't encounter any issues when we tried to redirect the user. But that's not always the case when adding redirections. By default, Angular matches paths by prefix. That means, that the following route will match both /recipes  and just /
+  // { path: '', redirectTo: '/somewhere-else' }
+  // Actually, Angular will give you an error here, because that's a common gotcha: This route will now ALWAYS redirect you! Why? Since the default matching strategy is "prefix" , Angular checks if the path you entered in the URL does start with the path specified in the route. Of course every path starts with ''  (Important: That's no whitespace, it's simply "nothing"). To fix this behavior, you need to change the matching strategy to "full" :
+  // { path: '', redirectTo: '/somewhere-else', pathMatch: 'full' }
+  // Now, you only get redirected, if the full path is ''  (so only if you got NO other content in your path in this example).
 ]
 
 @NgModule({
