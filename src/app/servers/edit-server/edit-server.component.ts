7548760c08171729308bdcc1397a4bc36ca9b763
Jenny Lewis-Gandy <jennylewis-gandy@ip-172-17-5-20.eu-west-1.compute.internal>
2022-09-12 15:34:29 +0100

    controlling navigation with candeactivate - change id update to a subscribe instead of a snapshot

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -16,6 +16,7 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
   serverStatus = '';
   allowEdit = false;
   changesSaved = false;
+  id: number;
 
   constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
@@ -26,9 +27,18 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
       }
     ); // using subscribe on queryParams means they will be updated reactively. If we use snapshot it will only load the inital state. Same for fragment
     this.route.fragment.subscribe();
-    const id = +this.route.snapshot.params['id'];
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
     // Subscribe route params to update the id if params change
-    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = +params['id'];
+        this.server = this.serversService.getServer(id);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+      }
+    );
+
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
